0330bea1dedc3de5ad2f4b329ec2130c50714b46
g.ibrahim.r <gryffindor.ibrahim@icloud.com>
2021-08-08 23:38:58 +0300

    Initial Commit

---
+++ SeaMap/SeaMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaMapApp.swift
+//  SeaMap
+//
+//  Created by İbrahim Güler on 8.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SeaMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
